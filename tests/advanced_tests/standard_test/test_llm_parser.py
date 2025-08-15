16a065c17deef84f1f6735995ce3ee18f1eb3ad6
wangtianxiong <wangtianxiong@sensetime.com>
2025-08-15 11:59:51 +0800

    同步main分支

--- tests/advanced_tests/standard_test/test_llm_parser.py
+++ tests/advanced_tests/standard_test/test_llm_parser.py
@@ -4,6 +4,7 @@ from lazyllm import LLMParser, TrainableModule
 from lazyllm.launcher import cleanup
 from lazyllm.tools.rag import DocNode
 
+
 class TestLLMParser(unittest.TestCase):
     @classmethod
     def setup_class(cls):
